e3ef17372b9ca511f6a76155e07eca20de685349
Daphaz <bonnet.damien@ymail.com>
2021-02-23 14:00:00 +0100

    style(header): change mobile version & add Link

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,9 +16,6 @@ export const Header = () => {
 	const handleLogo = () => {
 		router.push("/");
 	};
-	const handleLogout = () => {
-		logout();
-	};
 	return (
 		<header className={styles.header}>
 			<div className={styles.logo} onClick={handleLogo}>
@@ -26,15 +23,38 @@ export const Header = () => {
 			</div>
 			<nav className={styles.navbar}>
 				<ul>
-					<li className={styles.navlinks}>
-						<a href="#">Explorer</a>
-					</li>
-					<li className={styles.navlinks}>
-						<a href="#">Utiliser</a>
-					</li>
-					<li className={styles.navlinks}>
-						<a href="#">Contact</a>
-					</li>
+					{!isAuthenticated ? (
+						<>
+							<li className={styles.navlinks}>
+								<Link href="/explorer">
+									<a>Explorer</a>
+								</Link>
+							</li>
+							<li className={styles.navlinks}>
+								<Link href="/utiliser">
+									<a>Utiliser</a>
+								</Link>
+							</li>
+							<li className={styles.navlinks}>
+								<Link href="/contact">
+									<a>Contact</a>
+								</Link>
+							</li>
+						</>
+					) : (
+						<>
+							<li className={styles.navlinks}>
+								<Link href="/preparation">
+									<a>Preparation</a>
+								</Link>
+							</li>
+							<li className={styles.navlinks}>
+								<Link href="programmer">
+									<a>Programmer</a>
+								</Link>
+							</li>
+						</>
+					)}
 					{isAuthenticated && user.role === "admin" && (
 						<li>
 							<Link href="/dashboard">
@@ -73,22 +93,49 @@ export const Header = () => {
 				<span></span>
 			</div>
 			<div className={styles.menu} ref={menuOpen}>
-				<div className="bgMenu"></div>
+				<div className="bgMenu">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+						<path
+							fillOpacity="1"
+							d="M0,96L48,112C96,128,192,160,288,154.7C384,149,480,107,576,117.3C672,128,768,192,864,229.3C960,267,1056,277,1152,250.7C1248,224,1344,160,1392,128L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+					</svg>
+				</div>
 				<nav>
 					<ul>
-						<li>
-							<a href="#">Explorer</a>
-						</li>
-						<li>
-							<a href="#">Utiliser</a>
-						</li>
-						<li>
-							<a href="#">Contact</a>
-						</li>
-						{isAuthenticated && (
-							<li onClick={logout}>
-								<span>Deconnexion</span>
-							</li>
+						{!isAuthenticated ? (
+							<>
+								<li>
+									<Link href="/explorer">
+										<a className={styles.navlinks}>Explorer</a>
+									</Link>
+								</li>
+								<li>
+									<Link href="/utiliser">
+										<a className={styles.navlinks}>Utiliser</a>
+									</Link>
+								</li>
+								<li>
+									<Link href="/contact">
+										<a className={styles.navlinks}>Contact</a>
+									</Link>
+								</li>
+							</>
+						) : (
+							<>
+								<li>
+									<Link href="/preparation">
+										<a className={styles.navlinks}>Preparation</a>
+									</Link>
+								</li>
+								<li>
+									<Link href="programmer">
+										<a className={styles.navlinks}>Programmer</a>
+									</Link>
+								</li>
+								<li className={styles.navlinks} onClick={logout}>
+									<span>Deconnexion</span>
+								</li>
+							</>
 						)}
 						{isAuthenticated && user.role === "admin" && (
 							<li>
